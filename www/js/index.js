5910a0a47ec71c1a7e2bf1ccc1cea30ad10a3f8b
“Leo <lw@kju.at>
2017-12-05 16:00:52 +0100

    approval version

--- www/js/index.js
+++ www/js/index.js
@@ -1,53 +1,63 @@
 ﻿
-var texts= {
-	"conerr": {"de-CH": "Bitte überprüfen Sie die Internetverbindung!",
-			 "fr-CH": "Merci de vérifier votre connexion Internet!",
-			 "it-CH": "Controllare la connessione Internet!"},
+var texts = {
+    "conerr": {
+        "de-CH": "Bitte überprüfen Sie die Internetverbindung!",
+        "fr-CH": "Merci de vérifier votre connexion Internet!",
+        "it-CH": "Controllare la connessione Internet!"
+    },
 }
 
 var app = {
     // Application Constructor
-	lang: "",
-	percent: -1,
-	firstTime: true,
-	progressToaster: 0,
-    $this:null,
+    lang: "",
+    percent: -1,
+    firstTime: true,
+    progressToaster: 0,
+    $this: null,
 
-    initialize: function() {
+    initialize: function () {
         $this = this;
-		this.lang = navigator.language.slice(0,2);
-	
-		if (this.lang == "de" || this.lang == "it" || this.lang == "fr")
-			this.lang = this.lang + "-CH";
-		else 
-			this.lang = "fr-CH";
+        this.lang = navigator.language.slice(0, 2);
+
+        if (this.lang == "de" || this.lang == "it" || this.lang == "fr")
+            this.lang = this.lang + "-CH";
+        else
+            this.lang = "fr-CH";
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
         document.addEventListener("online", this.onOnline, false);
-	},
+        document.addEventListener("offline", function () { app.toast("conerr"); }, false);
+    },
 
-    onDeviceReady: function() {
-		 if (navigator.connection.type == Connection.NONE) {
-              navigator.notification.alert(texts.connerr[$this.lang]);
-            } else {
-               $this.onOnline();
+    onDeviceReady: function () {
+        navigator.splashscreen.show();
+        if (navigator.connection.type == Connection.NONE) {
+            app.toast("conerr");
+        } else {
+            $this.onOnline();
         }
     },
-    onOnline : function() {
-        navigator.splashscreen.hide();
-        //document.location="http://ch-co2tieferlegen.preview.kju.at/co2tl_app/index.html?disableauth=1";
-        var assets = window.location.toString().split('/').slice(0,-2).join('/');
-        document.getElementById('url').value = "http://192.168.1.42:59595/co2tl_app/index.html?assets=" + encodeURIComponent(assets);
-        document.getElementById('go').onclick=function() {
-         document.location = document.getElementById('url').value
-        }
-    }
+    getString: function (ident) {
+        var t = texts[ident]
+        return t[app.lang];
+    },
+    onOnline: function () {
+        var assets = window.location.toString().split('/').slice(0, -2).join('/');
+        document.location = "https://co2tieferlegen.ch/co2tl_app/index.html?assets=" + encodeURIComponent(assets); // document.getElementById('url').value
+        window.setTimeout(function () { document.location.reload(); }, 1000);
+    },
+    exit: function () {
+        navigator.app.exitApp();
+    },
+    toast: function (identifier, length) {
+        window.plugins.toast.show(app.getString(identifier), length ? length : "long", 'bottom', function (a) { console.log('toast success: ' + a) }, function (b) { console.log('toast error: ' + b) });
+    },
 
 }
 
